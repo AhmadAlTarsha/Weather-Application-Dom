f183e2b85143da86b4b49e70129fa16ce70eca41
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2023-12-19 10:38:32 +0300

    write sodo code

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ const body =document.querySelector("body")
 // this fun return an object (coords) contains a lot of proparety i think i need (latitude,longitude)
 // finally to git the weather from API i need to compare the lat and long that i have from the geolocation fun
 // and return the object from API that match my coord to git the right one 
-
+//-------
+//ok i think i can solve it In two ways: Either by tracking the user's location and making it a dynamic process by
+//using navigator.geolocation.watchPosition;
+//or I get the user's current location and when his done when he out and want use the website agin
+//call the function agin to git his position
 const button=document.createElement("button")
 body.append(button)
 button.innerText="get my location"
